fe4562cd42ccbcbd16c8eec39e2a87cbc45e6727
zhangkai.jacky <deathlord@163.com>
2019-02-12 15:22:58 +0800

    fix: 说明错误

--- src/index.d.ts
+++ src/index.d.ts
@@ -6,7 +6,7 @@ interface CodeSnap {
 /**
  * 将代码插入到指定的位置中
  * @param  {string} sourceCode 被插入代码的源码
- * @param  {Array<CodeSnap>} codeSnaps 代码片段，格式： { code: 'import xxx from'xxx';', slot: 'import' }。会将sourcecode里对应slot的地方插入代码
+ * @param  {Array<CodeSnap>} codeSnaps 代码片段，格式： { code: 'import xxx from'xxx';', slotName: 'import' }。会将sourcecode里对应slot的地方插入代码
  * @param  {babel.ParserOptions} babelOptions 最好将当前项目的babelconfig传过来，避免解析错误
  * @param  {prettier.Options} prettierConfig? 默认会读取当前项目的".prettierrc"文件，也可以自己传入
  * @returns 转换后的代码 string
